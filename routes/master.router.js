592b716cb10fb5ad2505b4e13402bc5597bbcbfc
AlexKiz <alexauvrus@gmail.com>
2021-09-06 14:05:05 +0300

    create master validation

--- routes/master.router.js
+++ routes/master.router.js
@@ -1,14 +1,9 @@
 const Router = require('express')
 const router = new Router()
-const {postMaster, getMaster, getAvailableMasters, getAvailableMastersForUpdate, putMaster, deleteMaster} = require('../controller/master.controller')
+const {getAvailableMasters} = require('../controller/master.controller')
 
-router.post('/master', postMaster)
-router.get('/master', getMaster)
+
 router.get('/availableMasters', getAvailableMasters)
-router.get('/availableMastersforUpdate', getAvailableMastersForUpdate)
-router.put('/master', putMaster)
-router.delete('/master', deleteMaster)
 
 
-
-module.exports = router
+module.exports = router 
