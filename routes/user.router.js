c532b9695833e257db37fbcd9e249e85d77fd31c
AlexKiz <alexauvrus@gmail.com>
2021-09-06 14:06:20 +0300

    create user validation

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,12 +1,10 @@
 const Router = require('express')
 const router = new Router()
-const {postUser, getUser, putUser, deleteUser} = require('../controller/user.controller')
-
-router.post('/user', postUser)
-router.get('/user', getUser)
-router.put('/user', putUser)
-router.delete('/user', deleteUser)
+const {postUser} = require('../controller/user.controller')
+const {postUserValidate} = require('../controller/user.validate')
 
 
+router.post('/user', [postUserValidate], postUser)
 
-module.exports = router
+
+module.exports = router 
