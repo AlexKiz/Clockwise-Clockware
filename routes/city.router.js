07a763db01b7c9f0ae9d02c339435a379aa2a300
AlexKiz <alexauvrus@gmail.com>
2021-09-06 14:06:58 +0300

    create private routes with validation

--- routes/city.router.js
+++ routes/city.router.js
@@ -2,11 +2,8 @@ const Router = require('express')
 const router = new Router()
 const {postCity, getCity, putCity, deleteCity} = require('../controller/city.controller')
 
-router.post('/city', postCity)
+
 router.get('/city', getCity)
-router.put('/city', putCity)
-router.delete('/city', deleteCity)
 
 
-
-module.exports = router
+module.exports = router 
